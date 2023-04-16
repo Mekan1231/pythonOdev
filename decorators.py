8e665c439748189415cd8ccb38564d4b2b1efab7
Mekan <l1812804309@ogr.sdu.edu.tr>
2023-04-16 23:06:26 +0300

    6.gun odev sonu

--- decorators.py
+++ decorators.py
@@ -1,15 +1,16 @@
 
-def printer():
-    print("hello world")
+
 
 def display_info(func):
     def inner():
         print("Exacuting",func.__name__,"function")
         func()
-        print("Finished exacution")
+        print("Finished exacution".upper())
     return inner
 
+@display_info
+def printer():
+    print("hello world")
 
 
-result= display_info(printer)
-result()
+printer()
